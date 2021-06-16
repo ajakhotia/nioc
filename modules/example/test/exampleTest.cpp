2663871b41e7aa1ea62bcea7921ba37cd0a0d470
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-06-16 15:24:49 +0530

    feat(example): add test to example module

---
+++ modules/example/test/exampleTest.cpp
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2021.                                                                                                 /
+// Project  : Naksh                                                                                                    /
+// Author   : Anurag Jakhotia                                                                                          /
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <naksh/example/example.hpp>
+#include <gtest/gtest.h>
+
+namespace naksh::example
+{
+
+TEST(ExampleTest, Construction)
+{
+    const Example example("TestExample");
+    EXPECT_EQ(example.name(), "TestExample");
+}
+
+
+TEST(ExampleTest, CopyConstruction)
+{
+    const Example example("anotherExample");
+    const auto copy = example;
+
+    EXPECT_EQ(copy.name(), "anotherExample");
+}
+
+
+TEST(ExampleTest, MoveConstruction)
+{
+    Example e1("yetAnotherExample");
+
+    // NOTE(Move semantics):    Variable e1 is consumed in the process of move-construction of e2. e2 steals
+    //                          underlying std::string as it has a efficient move-copy and move-assignment.
+    //                          As a result, e1.name() returns an empty string. This is why one should not
+    //                          use an l-value after calling move on it.
+    const auto e2 = std::move(e1);
+
+    EXPECT_EQ(e2.name(), "yetAnotherExample");
+    EXPECT_EQ(e1.name(), "");
+}
+
+} // End of namespace naksh::example.
