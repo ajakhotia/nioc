941f861859c025615d28ab11ff2f283c58cb39be
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-10-25 00:29:53 +0200

    feat(build): add function to add and export executables

--- cmake/addTargets.cmake
+++ cmake/addTargets.cmake
@@ -59,3 +59,46 @@ function(add_exported_library)
     install(TARGETS ${AEL_PARAM_TARGET} EXPORT ${AEL_PARAM_EXPORT})
 
 endfunction()
+
+
+function(add_exported_executable)
+
+    set(OPTIONS_ARGUMENTS "")
+
+    set(SINGLE_VALUE_ARGUMENTS
+            TARGET
+            TYPE
+            NAMESPACE
+            EXPORT)
+
+    set(MULTI_VALUE_ARGUMENTS
+            SRC_FILES
+            PRIVATE_INCLUDE_DIR
+            PRIVATE_LINK_LIBRARIES
+            PRIVATE_HEADERS
+            COMPILE_FEATURES
+            COMPILE_OPTIONS)
+
+
+    cmake_parse_arguments("AEL_PARAM"
+            "${OPTIONS_ARGUMENTS}"
+            "${SINGLE_VALUE_ARGUMENTS}"
+            "${MULTI_VALUE_ARGUMENTS}"
+            ${ARGN})
+
+
+    add_executable(${AEL_PARAM_TARGET} ${AEL_PARAM_SRC_FILES})
+
+    if(AEL_PARAM_NAMESPACE)
+        add_executable(${AEL_PARAM_NAMESPACE}${AEL_PARAM_TARGET} ALIAS ${AEL_PARAM_TARGET})
+    endif()
+
+    target_include_directories(${AEL_PARAM_TARGET} PRIVATE ${AEL_PARAM_PRIVATE_INCLUDE_DIR})
+    target_link_libraries(${AEL_PARAM_TARGET} PRIVATE ${AEL_PARAM_PRIVATE_LINK_LIBRARIES})
+
+    target_compile_features(${AEL_PARAM_TARGET} PRIVATE ${AEL_PARAM_COMPILE_FEATURES})
+    target_compile_options(${AEL_PARAM_TARGET} PRIVATE ${AEL_PARAM_COMPILE_OPTIONS})
+
+    install(TARGETS ${AEL_PARAM_TARGET} EXPORT ${AEL_PARAM_EXPORT})
+
+endfunction()
