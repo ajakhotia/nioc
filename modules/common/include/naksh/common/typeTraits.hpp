04909f4d4e3d988c1b8e6dd777d5612e17099fa1
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-11-20 16:13:18 +0100

    refactor(common): improve parameter naming and add docs

--- modules/common/include/naksh/common/typeTraits.hpp
+++ modules/common/include/naksh/common/typeTraits.hpp
@@ -13,20 +13,39 @@
 namespace naksh::common
 {
 
+/// @brief  SFINAE based helper type to determine if the InstanceType is template
+///         specialization of the TemplateType. This declaration represents the
+///         general case when an InstanceType is not actually a specialization of
+///         TemplateType, hence, the inheritance from the std::false_type.
+/// @tparam InstanceType    Type of the instance to be checked. Eg: std::vector<int>.
+/// @tparam TemplateType    Type of the template. Eg: std::vector.
 template<typename InstanceType, template <typename ...> typename TemplateType>
 struct IsSpecialization : public std::false_type
 {
 };
 
 
-template< template<typename ...> typename Template, typename ...Args>
-struct IsSpecialization<Template<Args...>, Template> : public std::true_type
+/// @brief  Specialization of @class IsSpecialization for the case when the
+///         InstanceType is actually and instance of the TemplateType. Hence,
+///         the inheritance from std::true_type.
+/// @tparam TemplateType    The template type, Eg: std::vector
+/// @tparam Args            ArgumentsType that specialize the template type.
+///                         Eg: <int, ...> for the case of std::vector<int>.
+///
+///                         The ... is for other parameter that may be needed
+///                         by the template, such as allocator type in case of
+///                         std::vector<>.
+template< template<typename ...> typename TemplateType, typename ...Args>
+struct IsSpecialization<TemplateType<Args...>, TemplateType> : public std::true_type
 {
 };
 
 
-template<typename LhsType, template <typename...> typename RhsType>
-inline constexpr bool isSpecialization = IsSpecialization<LhsType, RhsType>::value;
+/// @brief  A helper to variable check semantics for @class IsSpecialization.
+/// @tparam InstanceType    Type of the instance. Eg: std::vector<int>
+/// @tparam TemplateType    Type of the template. Eg: std::vector.
+template<typename InstanceType, template <typename...> typename TemplateType>
+inline constexpr bool isSpecialization = IsSpecialization<InstanceType, TemplateType>::value;
 
 
 /// @brief  Function to get a human readable name of a type.
