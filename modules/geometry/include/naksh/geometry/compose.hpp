a879bf38a8f9129750bd09f4c08318e46e6a12c0
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-11-20 18:42:41 +0100

    feat(geometry): add basis for tranform composition

--- modules/geometry/include/naksh/geometry/compose.hpp
+++ modules/geometry/include/naksh/geometry/compose.hpp
@@ -14,7 +14,12 @@ namespace naksh::geometry
 template<typename LhsTransform, typename RhsTransform>
 class ComposeTransform
 {
+    static_assert(
+        FramesEqual<typename LhsTransform::ChildFrame, typename RhsTransform::ParentFrame>::value(),
+        "ChildFrame of Lhs does not match the ParentFrame for Rhs. The transforms "
+        "cannot be composed together.");
 
+    using ResultTransform = Transform<typename LhsTransform::ParentFrame, typename RhsTransform::ChildFrame>;
 };
 
 } // End of namespace naksh::geometry.
---
+++ modules/geometry/test/composeTest.cpp
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2021.                                                                                                 /
+// Project  : NakshOps                                                                                                 /
+// Author   : Anurag Jakhotia                                                                                          /
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+#include <naksh/geometry/compose.hpp>
+#include <gtest/gtest.h>
+
+namespace naksh::geometry
+{
+
+TEST(ComposeTransform, Construction)
+{
+
+}
+
+
+} // End of namespace naksh::geometry.
+
+#pragma clang diagnostic pop
