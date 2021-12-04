d4c1cdf6ab713842c0ac319f0db0204388253943
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-12-04 16:04:50 +0100

    fix(common): improve caching tests

--- modules/common/test/cacheManagerTest.cpp
+++ modules/common/test/cacheManagerTest.cpp
@@ -15,12 +15,14 @@ namespace naksh::common
 namespace
 {
 
+std::string gTestCacheMessage;
+
 class TestCache : public CacheBase<int>
 {
 public:
     explicit TestCache(int value) : mCached(value)
     {
-        std::cout << "New test cache created with value: " << mCached << std::endl;
+        gTestCacheMessage = "Created cache for: " + std::to_string(mCached);
     }
 
     [[nodiscard]] bool valid(const int& value) const noexcept override
@@ -32,12 +34,12 @@ public:
     {
         if (not mSquare)
         {
-            std::cout << "Computing square for value: " << mCached << std::endl;
+            gTestCacheMessage = "Computing for value: " + std::to_string(mCached);
             mSquare = std::pow(mCached, 2);
         }
         else
         {
-            std::cout << "Reusing old results for value: " << mCached << std::endl;
+            gTestCacheMessage = "Reusing for value: " + std::to_string(mCached);
         }
 
         return *mSquare;
@@ -82,12 +84,14 @@ private:
 TEST(TestCache, construction)
 {
     EXPECT_NO_THROW(const auto testCache = TestCache(7));
+    EXPECT_EQ(gTestCacheMessage, "Created cache for: 7");
 }
 
 
 TEST(TestCache, valid)
 {
     const auto testCache = TestCache(13);
+    EXPECT_EQ(gTestCacheMessage, "Created cache for: 13");
     EXPECT_TRUE(testCache.valid(13));
     EXPECT_FALSE(testCache.valid(0));
 }
@@ -96,13 +100,22 @@ TEST(TestCache, valid)
 TEST(TestCache, squareArea)
 {
     auto testCache = TestCache(17);
+
+    EXPECT_EQ(gTestCacheMessage, "Created cache for: 17");
+
     EXPECT_EQ(289, testCache.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Computing for value: 17");
+
+    EXPECT_EQ(289, testCache.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 17");
 }
 
 
 TEST(CacheManager, construction)
 {
+    gTestCacheMessage = "";
     EXPECT_NO_THROW(CacheManager<TestCache> cacheManager);
+    EXPECT_EQ(gTestCacheMessage, "");
 }
 
 
@@ -111,24 +124,32 @@ TEST(CacheManager, access)
     CacheManager<TestCache> cacheManager;
     {
         auto& cacheRef = cacheManager.access(19);
+
+        EXPECT_EQ(gTestCacheMessage, "Created cache for: 19");
         EXPECT_EQ(361, cacheRef.squareArea());
+        EXPECT_EQ(gTestCacheMessage, "Computing for value: 19");
     }
 
     {
         auto& cacheRef = cacheManager.access(19);
         EXPECT_EQ(361, cacheRef.squareArea());
+        EXPECT_EQ(gTestCacheMessage, "Reusing for value: 19");
     }
 
     {
         auto& cacheRef = cacheManager.access(23);
+        EXPECT_EQ(gTestCacheMessage, "Created cache for: 23");
         EXPECT_EQ(529, cacheRef.squareArea());
+        EXPECT_EQ(gTestCacheMessage, "Computing for value: 23");
     }
 }
 
 
 TEST(Caching, construction)
 {
+    gTestCacheMessage = "";
     EXPECT_NO_THROW(TestClass(29));
+    EXPECT_EQ(gTestCacheMessage, "");
 }
 
 
@@ -137,22 +158,38 @@ TEST(Caching, squareArea)
     TestClass testClass(31);
     auto& internalIntRef = testClass.acquireReference();
 
-    std::cout << "Finished creation of test class. Computing squareArea from here on." << std::endl;
+    EXPECT_EQ(gTestCacheMessage, "");
+
     EXPECT_EQ(961, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Computing for value: 31");
+
     EXPECT_NE(800, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 31");
+
     EXPECT_EQ(961, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 31");
+
 
     internalIntRef = 37;
-    std::cout << "Sneakily set value to 37." << std::endl;
     EXPECT_EQ(1369, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Computing for value: 37");
+
     EXPECT_NE(1000, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 37");
+
     EXPECT_EQ(1369, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 37");
+
 
     internalIntRef = 41;
-    std::cout << "Sneakily set value to 41." << std::endl;
     EXPECT_EQ(1681, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Computing for value: 41");
+
     EXPECT_NE(1000, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 41");
+
     EXPECT_EQ(1681, testClass.squareArea());
+    EXPECT_EQ(gTestCacheMessage, "Reusing for value: 41");
 }
 
 
