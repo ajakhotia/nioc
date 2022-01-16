032c8bd864c3259ab2af1405a624c3dc1e505847
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-01-16 18:48:21 +0100

    feat(geometry): add files for rigid transforms

---
+++ modules/geometry/test/rigidTransformTest.cpp
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022.                                                                             /
+// Project  : Naksh                                                                                /
+// Author   : Anurag Jakhotia                                                                      /
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+#include <naksh/geometry/rigidTransform.hpp>
+#include <gtest/gtest.h>
+
+namespace naksh::geometry
+{
+
+} // End of namespace naksh::geometry.
+
+#pragma clang diagnostic pop
