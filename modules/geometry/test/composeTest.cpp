0530fb1c21812fb87e4d2677b74f8df6f3d979e5
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-06 18:06:47 +0100

    improve(geometry): add tests

--- modules/geometry/test/composeTest.cpp
+++ modules/geometry/test/composeTest.cpp
@@ -103,6 +103,105 @@ TEST(ComposeTransform, StaticStaticStaticDynamic)
     EXPECT_EQ(result.childFrame().name(), "milkyWay");
 }
 
+
+TEST(ComposeTransform, StaticStaticDynamicStatic)
+{
+    using SunFromUranusFrames = FrameReferences<StaticFrame<Sun>, StaticFrame<Uranus>>;
+    using DynamicFromPlutoFrames = FrameReferences<DynamicFrame, StaticFrame<Pluto>>;
+
+    {
+        SunFromUranusFrames lhs;
+        DynamicFromPlutoFrames rhs("naksh::geometry::Uranus");
+
+        EXPECT_NO_THROW(composeFrameReferences(lhs, rhs));
+        const auto result = composeFrameReferences(lhs, rhs);
+
+        using ResultType = decltype(result);
+        static_assert(std::is_same_v<typename ResultType::ParentFrame, StaticFrame<Sun>>);
+        static_assert(std::is_same_v<typename ResultType::ChildFrame, StaticFrame<Pluto>>);
+    }
+
+    {
+        SunFromUranusFrames lhs;
+        DynamicFromPlutoFrames rhs("milkyWay");
+
+        EXPECT_THROW(composeFrameReferences(lhs, rhs), FrameCompositionException);
+    }
+}
+
+
+TEST(ComposeTransform, StaticDynamicStaticStatic)
+{
+    using SunFromDynamicFrames = FrameReferences<StaticFrame<Sun>, DynamicFrame>;
+    using DynamicFromPlutoFrames = FrameReferences<StaticFrame<Uranus>, StaticFrame<Pluto>>;
+
+    {
+        SunFromDynamicFrames lhs("naksh::geometry::Uranus");
+        DynamicFromPlutoFrames rhs;
+
+        EXPECT_NO_THROW(composeFrameReferences(lhs, rhs));
+        const auto result = composeFrameReferences(lhs, rhs);
+
+        using ResultType = decltype(result);
+        static_assert(std::is_same_v<typename ResultType::ParentFrame, StaticFrame<Sun>>);
+        static_assert(std::is_same_v<typename ResultType::ChildFrame, StaticFrame<Pluto>>);
+    }
+
+    {
+        SunFromDynamicFrames lhs("milkyWay");
+        DynamicFromPlutoFrames rhs;
+
+        EXPECT_THROW(composeFrameReferences(lhs, rhs), FrameCompositionException);
+    }
+}
+
+
+TEST(ComposeTransform, StaticDynamicDynamicStatic)
+{
+    using SunFromDynamicFrames = FrameReferences<StaticFrame<Sun>, DynamicFrame>;
+    using DynamicFromPlutoFrames = FrameReferences<DynamicFrame, StaticFrame<Pluto>>;
+
+    {
+        SunFromDynamicFrames lhs("naksh::geometry::Uranus");
+        DynamicFromPlutoFrames rhs("naksh::geometry::Uranus");
+
+        EXPECT_NO_THROW(composeFrameReferences(lhs, rhs));
+        const auto result = composeFrameReferences(lhs, rhs);
+
+        using ResultType = decltype(result);
+        static_assert(std::is_same_v<typename ResultType::ParentFrame, StaticFrame<Sun>>);
+        static_assert(std::is_same_v<typename ResultType::ChildFrame, StaticFrame<Pluto>>);
+    }
+
+    {
+        SunFromDynamicFrames lhs("MilkyWay");
+        DynamicFromPlutoFrames rhs("Andromeda");
+
+        EXPECT_THROW(composeFrameReferences(lhs, rhs), FrameCompositionException);
+    }
+}
+
+
+TEST(ComposeTransform, DynamicStaticStaticDynamic)
+{
+    using DynamicFromUranusFrames = FrameReferences<DynamicFrame, StaticFrame<Uranus>>;
+    using UranusFromDynamicFrames = FrameReferences<StaticFrame<Uranus>, DynamicFrame>;
+
+    {
+        DynamicFromUranusFrames lhs("naksh::geometry::Sun");
+        UranusFromDynamicFrames rhs("naksh::geometry::Pluto");
+
+        EXPECT_NO_THROW(composeFrameReferences(lhs, rhs));
+        const auto result = composeFrameReferences(lhs, rhs);
+
+        using ResultType = decltype(result);
+        static_assert(std::is_same_v<typename ResultType::ParentFrame, DynamicFrame>);
+        static_assert(std::is_same_v<typename ResultType::ChildFrame, DynamicFrame>);
+        EXPECT_EQ(result.parentFrame().name(), "naksh::geometry::Sun");
+        EXPECT_EQ(result.childFrame().name(), "naksh::geometry::Pluto");
+    }
+}
+
 } // End of namespace naksh::geometry.
 
 #pragma clang diagnostic pop
