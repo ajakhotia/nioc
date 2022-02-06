1ba343febaf9071eb51135925e4b9a9c9e76c031
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-06 12:57:53 +0100

    chore(geometry): format compose.hpp

--- modules/geometry/include/naksh/geometry/compose.hpp
+++ modules/geometry/include/naksh/geometry/compose.hpp
@@ -6,11 +6,12 @@
 #pragma once
 
 #include "frameReferences.hpp"
+
+#include <cassert>
 #include <naksh/common/typeTraits.hpp>
 #include <string>
-#include <cassert>
-#include <type_traits>
 #include <tuple>
+#include <type_traits>
 
 namespace naksh::geometry
 {
@@ -18,7 +19,7 @@ namespace naksh::geometry
 
 /// @brief  Error thrown when a mis-match of frame ids is detected during transform
 ///         composition at run-time.
-class FrameCompositionException : public std::runtime_error
+class FrameCompositionException: public std::runtime_error
 {
 public:
     /// Inherit all constructors from std::runtime_error.
@@ -27,14 +28,15 @@ public:
     /// @brief  Convenience constructor to appropriately build error message from frame names.
     /// @param lhsFrameName
     /// @param rhsFrameName
-    FrameCompositionException(const std::string & lhsFrameName, const std::string& rhsFrameName) noexcept:
+    FrameCompositionException(const std::string& lhsFrameName,
+                              const std::string& rhsFrameName):
         FrameCompositionException(
-            "Composed transforms with mismatched inner frames. Lhs child frame[" +
-            lhsFrameName + "] does not match the rhs parent frame[" + rhsFrameName + "].")
+            "Composed transforms with mismatched inner frames. Lhs child frame[" + lhsFrameName +
+            "] does not match the rhs parent frame[" + rhsFrameName + "].")
     {
         assert(lhsFrameName != rhsFrameName &&
-            "Detected creation of FrameCompositionException but with compatible frame ids. "
-            "This likely a programming error.");
+               "Detected creation of FrameCompositionException but with compatible frame ids. "
+               "This likely a programming error.");
     }
 };
 
@@ -47,12 +49,14 @@ namespace helpers
 ///         compile-time, no run-time work in needed for this assertion.
 /// @tparam LhsFrame
 /// @tparam RhsFrame
-template<typename LhsFrame, typename RhsFrame, typename = typename std::enable_if_t<
-        common::isSpecialization<LhsFrame, StaticFrame> and
-        common::isSpecialization<RhsFrame, StaticFrame> and
-        LhsFrame::name() == RhsFrame::name()>>
+template<typename LhsFrame,
+         typename RhsFrame,
+         typename = typename std::enable_if_t<common::isSpecialization<LhsFrame, StaticFrame> and
+                                              common::isSpecialization<RhsFrame, StaticFrame> and
+                                              LhsFrame::name() == RhsFrame::name()>>
 inline constexpr void assertFrameEqual() noexcept
-{}
+{
+}
 
 
 /// @brief  Asserts equality of lhs and rhs frame for the case when Lhs is a StaticFrame<...>
@@ -60,9 +64,10 @@ inline constexpr void assertFrameEqual() noexcept
 /// @tparam LhsFrame
 /// @tparam RhsFrame
 /// @param rhsFrame
-template<typename LhsFrame, typename RhsFrame, typename = typename std::enable_if_t<
-        common::isSpecialization<LhsFrame, StaticFrame> and
-        std::is_same_v<RhsFrame, DynamicFrame>>>
+template<typename LhsFrame,
+         typename RhsFrame,
+         typename = typename std::enable_if_t<common::isSpecialization<LhsFrame, StaticFrame> and
+                                              std::is_same_v<RhsFrame, DynamicFrame>>>
 inline void assertFrameEqual(const RhsFrame& rhsFrame)
 {
     if(LhsFrame::name() != rhsFrame.name())
@@ -77,9 +82,10 @@ inline void assertFrameEqual(const RhsFrame& rhsFrame)
 /// @tparam LhsFrame
 /// @tparam RhsFrame
 /// @param lhsFrame
-template<typename LhsFrame, typename RhsFrame, typename = typename std::enable_if_t<
-        std::is_same_v<LhsFrame, DynamicFrame> and
-        common::isSpecialization<RhsFrame, StaticFrame>>>
+template<typename LhsFrame,
+         typename RhsFrame,
+         typename = typename std::enable_if_t<std::is_same_v<LhsFrame, DynamicFrame> and
+                                              common::isSpecialization<RhsFrame, StaticFrame>>>
 inline void assertFrameEqual(const LhsFrame& lhsFrame)
 {
     if(lhsFrame.name() != RhsFrame::name())
@@ -95,9 +101,10 @@ inline void assertFrameEqual(const LhsFrame& lhsFrame)
 /// @tparam RhsFrame
 /// @param lhsFrame
 /// @param rhsFrame
-template<typename LhsFrame, typename RhsFrame, typename = typename std::enable_if_t<
-        std::is_same_v<LhsFrame, DynamicFrame> and
-        std::is_same_v<RhsFrame, DynamicFrame>>>
+template<typename LhsFrame,
+         typename RhsFrame,
+         typename = typename std::enable_if_t<std::is_same_v<LhsFrame, DynamicFrame> and
+                                              std::is_same_v<RhsFrame, DynamicFrame>>>
 inline void assertFrameEqual(const LhsFrame& lhsFrame, const RhsFrame& rhsFrame)
 {
     if(lhsFrame.name() != rhsFrame.name())
@@ -119,16 +126,20 @@ inline void assertFrameEqual(const LhsFrame& lhsFrame, const RhsFrame& rhsFrame)
 /// @param rhsFrameReferences     Instance of the Rhs.
 /// @return FrameReferences that are a result of the composition.
 template<typename LhsFrameReferences, typename RhsFrameReferences>
-decltype(auto) composeFrameReferences(
-    const LhsFrameReferences& lhsFrameReferences, const RhsFrameReferences& rhsFrameReferences)
+FrameReferences<typename LhsFrameReferences::ParentFrame, typename RhsFrameReferences::ChildFrame>
+composeFrameReferences(const LhsFrameReferences& lhsFrameReferences,
+                       const RhsFrameReferences& rhsFrameReferences)
 {
-    std::apply(
-        helpers::assertFrameEqual<typename LhsFrameReferences::ChildFrame, typename RhsFrameReferences::ParentFrame>,
-        std::tuple_cat(lhsFrameReferences.childFrameAsTuple(), rhsFrameReferences.parentFrameAsTuple()));
+    using ResultFrameReference = FrameReferences<typename LhsFrameReferences::ParentFrame,
+                                                 typename RhsFrameReferences::ChildFrame>;
 
-    return std::make_from_tuple<
-        FrameReferences<typename LhsFrameReferences::ParentFrame, typename RhsFrameReferences::ChildFrame>>(
-            std::tuple_cat(lhsFrameReferences.parentFrameAsTuple(), rhsFrameReferences.childFrameAsTuple()));
+    std::apply(helpers::assertFrameEqual<typename LhsFrameReferences::ChildFrame,
+                                         typename RhsFrameReferences::ParentFrame>,
+               std::tuple_cat(lhsFrameReferences.childFrameAsTuple(),
+                              rhsFrameReferences.parentFrameAsTuple()));
+
+    return std::make_from_tuple<ResultFrameReference>(std::tuple_cat(
+        lhsFrameReferences.parentFrameAsTuple(), rhsFrameReferences.childFrameAsTuple()));
 }
 
 
