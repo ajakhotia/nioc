7fa97f373db1931e6104003c63ebc46657a51ecb
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-13 13:02:14 +0100

    chore(cmake): add clang-14 compilers

--- scripts/installSystemDeps-ubuntu2004.sh
+++ scripts/installSystemDeps-ubuntu2004.sh
@@ -8,4 +8,6 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 echo "deb https://apt.llvm.org/focal/ llvm-toolchain-focal-14 main" >  /etc/apt/sources.list.d/clang-14.list
 
 apt-get update
-apt-get install -y clang-format-14
+apt-get install -y      \
+    clang-14            \
+    clang-format-14
