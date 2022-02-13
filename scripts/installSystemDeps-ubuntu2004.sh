7584b44432f2823f6ba115083ca378e7cadafa3c
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-02-13 12:46:09 +0100

    feat(scripts): add script for sys deps

---
+++ scripts/installSystemDeps-ubuntu2004.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+####################################################################################################
+# Copyright (c) 2022.                                                                              #
+# Project  : Naksh                                                                                 #
+# Author   : Anurag Jakhotia                                                                       #
+####################################################################################################
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+echo "deb https://apt.llvm.org/focal/ llvm-toolchain-focal-14 main" >  /etc/apt/sources.list.d/clang-14.list
+
+apt-get update
+apt-get install -y clang-format-14
