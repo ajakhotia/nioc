2792a946c365eb94f169c5c3db3390608b78d74f
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-04-09 13:24:48 +0200

    refactor(geometry): clang format changes

--- modules/geometry/include/naksh/geometry/frameReferences.hpp
+++ modules/geometry/include/naksh/geometry/frameReferences.hpp
@@ -51,7 +51,8 @@ public:
         typename = typename std::enable_if_t<common::isSpecialization<ParentFrame, StaticFrame>>,
         typename = typename std::enable_if_t<std::is_same_v<ChildFrame, DynamicFrame>>>
     [[maybe_unused]] explicit FrameReferences(ChildConceptArgs&& childId) noexcept:
-        ParentConcept(), ChildConcept(std::forward<ChildConceptArgs>(childId))
+        ParentConcept(),
+        ChildConcept(std::forward<ChildConceptArgs>(childId))
     {
     }
 
@@ -69,7 +70,8 @@ public:
         typename = typename std::enable_if_t<std::is_same_v<ParentFrame, DynamicFrame>>,
         typename = typename std::enable_if_t<common::isSpecialization<ChildFrame, StaticFrame>>>
     [[maybe_unused]] explicit FrameReferences(ParentConceptArgs&& parentId, int = 0) noexcept:
-        ParentConcept(std::forward<ParentConceptArgs>(parentId)), ChildConcept()
+        ParentConcept(std::forward<ParentConceptArgs>(parentId)),
+        ChildConcept()
     {
     }
 
