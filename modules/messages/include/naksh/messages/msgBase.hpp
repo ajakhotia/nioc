19d7b04d2a04b466e0d603c8c52148e2d9458579
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2022-07-23 03:49:30 +0200

    feat(messages): add ability to write and read from log

--- modules/messages/include/naksh/messages/msgBase.hpp
+++ modules/messages/include/naksh/messages/msgBase.hpp
@@ -5,22 +5,68 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+#include <capnp/serialize.h>
 #include <cstdint>
+#include <naksh/logger/logger.hpp>
+#include <naksh/logger/memoryCrate.hpp>
+#include <variant>
 
 namespace naksh::messages
 {
+
+class MMappedMessageReader final: public logger::MemoryCrate, public capnp::FlatArrayMessageReader
+{
+public:
+    explicit MMappedMessageReader(logger::MemoryCrate memoryCrate);
+
+    MMappedMessageReader(const MMappedMessageReader&) = delete;
+
+    MMappedMessageReader(MMappedMessageReader&&) = delete;
+
+    ~MMappedMessageReader() final = default;
+
+    MMappedMessageReader& operator=(const MMappedMessageReader&) = delete;
+
+    MMappedMessageReader& operator=(MMappedMessageReader&&) = delete;
+};
+
+
 class MsgBase
 {
 public:
-    using MsgHandle = uint64_t;
+    using MsgHandle = std::uint64_t;
 
-    virtual ~MsgBase();
+    using Variant = std::variant<capnp::MallocMessageBuilder, MMappedMessageReader>;
+
+protected:
+    MsgBase();
+
+    explicit MsgBase(logger::MemoryCrate memoryCrate);
+
+public:
+    MsgBase(const MsgBase&) = delete;
+
+    MsgBase(MsgBase&&) noexcept = delete;
+
+    virtual ~MsgBase() = default;
+
+    MsgBase& operator=(const MsgBase&) = delete;
+
+    MsgBase& operator=(MsgBase&&) noexcept = delete;
 
     [[nodiscard]] virtual MsgHandle msgHandle() const = 0;
 
 protected:
-    MsgBase();
+    friend void write(MsgBase&, logger::Logger&);
+
+    [[nodiscard]] Variant& variant() noexcept;
+
+private:
+    Variant mVariant;
 };
+
+
+void write(MsgBase& msgBase, logger::Logger& logger);
 
 
 } // namespace naksh::messages
