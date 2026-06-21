2ac46d1c457a4b7323a2bac8a5cd0642c61ff6c3
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2026-06-21 02:57:50 +0200

    fix(chronicle): clean up utils

--- modules/chronicle/src/utils.cpp
+++ modules/chronicle/src/utils.cpp
@@ -4,101 +4,29 @@
 // Author   : Anurag Jakhotia
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #include "utils.hpp"
-#include <bit>
-#include <boost/endian.hpp>
-#include <fstream>
-#include <numeric>
+#include <algorithm>
 
 namespace nioc::chronicle
 {
 
-std::string padString(const std::string& input, const uint64_t paddedLength, const char paddingChar)
+std::string padString(
+    const std::string& input,
+    const std::uint64_t paddedLength,
+    const char paddingChar)
 {
   return std::string(paddedLength - std::min(paddedLength, input.size()), paddingChar) + input;
 }
 
 std::string buildRollName(const std::uint64_t rollId)
 {
-  return kRollFileNamePrefix + padString(std::to_string(rollId), kPaddedRollNumberLength) +
-         kRollFileNameExtension;
+  return kRollFileNamePrefix + padString(std::to_string(rollId), kPaddedNumberLength) +
+         kFileNameExtension;
 }
 
-bool fileHasSpace(
-    std::ofstream& file,
-    const std::uint64_t spaceRequired,
-    const std::uint64_t maxFileSizeInBytes)
+std::string buildTimelineFileName(const std::uint64_t fileIndex)
 {
-  if(spaceRequired > maxFileSizeInBytes)
-  {
-    common::throwException<std::invalid_argument>(
-        "Space requested on a file is greater than the maximum allowed size of the file. This is "
-        "an impossible constraint to satisfy.");
-  }
-
-  return (maxFileSizeInBytes - file.tellp()) >= spaceRequired;
-}
-
-std::uint64_t computeTotalSizeInBytes(
-    const std::span<const std::span<const std::byte>> dataCollection)
-{
-  return std::accumulate(
-      dataCollection.begin(),
-      dataCollection.end(),
-      0ULL,
-      [](const std::uint64_t accumulatedSize, const std::span<const std::byte>& data)
-      { return accumulatedSize + data.size_bytes(); });
-}
-
-template<>
-void ReadWriteUtil<SequenceEntry>::write(std::ostream& stream, const SequenceEntry value)
-{
-  boost::endian::native_to_little_inplace(value.mChannelId.mValue);
-  stream.write(std::bit_cast<const char*>(&value), sizeof(value));
-}
-
-template<>
-SequenceEntry ReadWriteUtil<SequenceEntry>::read(const char* ptr, std::uint64_t /*unused*/)
-{
-  auto value = *std::bit_cast<const SequenceEntry*>(ptr);
-  boost::endian::little_to_native_inplace(value.mChannelId.mValue);
-  return value;
-}
-
-template<>
-void ReadWriteUtil<IndexEntry>::write(std::ostream& stream, const IndexEntry value)
-{
-  boost::endian::native_to_little_inplace(value.mRollId);
-  boost::endian::native_to_little_inplace(value.mOffset);
-  boost::endian::native_to_little_inplace(value.mSize);
-
-  stream.write(std::bit_cast<const char*>(&value), sizeof(value));
-}
-
-template<>
-IndexEntry ReadWriteUtil<IndexEntry>::read(const char* ptr, const std::uint64_t /*unused*/)
-{
-  auto value = *std::bit_cast<const IndexEntry*>(ptr);
-
-  boost::endian::little_to_native_inplace(value.mRollId);
-  boost::endian::little_to_native_inplace(value.mOffset);
-  boost::endian::little_to_native_inplace(value.mSize);
-  return value;
-}
-
-template<>
-void ReadWriteUtil<std::span<const std::byte>>::write(
-    std::ostream& stream,
-    const std::span<const std::byte> value)
-{
-  stream.write(std::bit_cast<const char*>(value.data()), std::ssize(value));
-}
-
-template<>
-std::span<const std::byte> ReadWriteUtil<std::span<const std::byte>>::read(
-    const char* ptr,
-    const std::uint64_t size)
-{
-  return std::as_bytes(std::span(ptr, size));
+  return kTimelineFileNamePrefix + padString(std::to_string(fileIndex), kPaddedNumberLength) +
+         kFileNameExtension;
 }
 
 } // namespace nioc::chronicle
